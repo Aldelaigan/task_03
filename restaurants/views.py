ed5f856f1d189af8d51273332ab2ce6bafddcfae
Abdulrahman Aldelaigan <a.aldelaigan@gmail.com>
2018-07-03 19:00:55 +0300

    Working list and details pages

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,14 +6,34 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+        "my_list": [
+            {
+                "restaurant_name":"Burger King",
+                "food_type":"burger",
+            },
+            {
+                "restaurant_name":"Pizza Hut",
+                "food_type":"burger",
+            },
+            {
+                "restaurant_name":"McDonalds",
+                "food_type":"nuggets",
+            },
+        ],
     }
+
+
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
     context = {
-
+        "my_object": {
+            "restaurant_name":"Burger King",
+            "food_type":"burger",
+            "ingredients":"mayonnaise ketchup meat",
+            "delivery":"Yes",
+        },
     }
     return render(request, 'detail.html', context)
